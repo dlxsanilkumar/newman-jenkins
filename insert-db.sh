0c9fb8420ba9dccb0a9846142758bbac7f314328
Anil Kumar N <anil.n.kumar@delixus.com>
2021-12-07 23:46:58 +0530

    Modified insert.sh file

--- insert-db.sh
+++ insert-db.sh
@@ -10,6 +10,7 @@ case $GET_VULN_DATA in
 
 	 echo "--- insert into build_ids table ---"
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'FAILURE');";
+	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM solidityscans ORDER BY scanid DESC LIMIT 1";
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1"; exit 1;
 	 ;;
      *)
@@ -17,6 +18,7 @@ case $GET_VULN_DATA in
 	 
 	 echo "--- insert into build_ids table ---"
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'SUCCESS');";
+	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM solidityscans ORDER BY scanid DESC LIMIT 1";
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1"; exit 0;
 	 ;;
 esac
