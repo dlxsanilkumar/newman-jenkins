e5d62d185749faf10de930137efbe7f6eb953e83
Anil Kumar N <anil.n.kumar@delixus.com>
2021-12-08 14:31:10 +0530

    Scan with odd_even.sol

--- upload-solidity-files.sh
+++ upload-solidity-files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# Copy files to the server.
+# Copy the solidiy files to the server.
 scp -i ~/.ssh/mythril-scan-key.pem ~/Migrations.sol ubuntu@13.126.95.15:/data/delixus/;
 scp -i ~/.ssh/mythril-scan-key.pem ~/Adoption.sol ubuntu@13.126.95.15:/data/delixus/;
+scp -i ~/.ssh/mythril-scan-key.pem ~/odd_even.sol ubuntu@13.126.95.15:/data/delixus/;
