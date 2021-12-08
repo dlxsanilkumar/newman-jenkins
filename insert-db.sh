22b2635b6f236bb740a103fa610f43a9c0bba608
Anil Kumar N <anil.n.kumar@delixus.com>
2021-12-08 11:18:42 +0530

    Added database error handling.

--- insert-db.sh
+++ insert-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT result FROM solidityscans ORDER BY scanid DESC LIMIT 1" > db.log;
-GET_VULN_DATA=`awk 'FNR == 3 {print $1}' db.log;`
+psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT result FROM solidityscans ORDER BY scanid DESC LIMIT 1" > result.log 2>&1;
+GET_VULN_DATA=`awk 'FNR == 3 {print $1}' result.log;`
 
 case $GET_VULN_DATA in
      *vulnPresent*) 
@@ -9,7 +9,7 @@ case $GET_VULN_DATA in
 	 echo "See the details below for more information."
 
 	 echo "--- insert into build_ids table ---"
-	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'FAILURE');";
+	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'FAILURE');" > db.log 2>&1;
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM solidityscans ORDER BY scanid DESC LIMIT 1";
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1"; exit 1;
 	 ;;
@@ -17,8 +17,20 @@ case $GET_VULN_DATA in
      echo Build is completed successfully...
 	 
 	 echo "--- insert into build_ids table ---"
-	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'SUCCESS');";
+	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'SUCCESS');" > db.log 2>&1;
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM solidityscans ORDER BY scanid DESC LIMIT 1";
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1"; exit 0;
 	 ;;
 esac
+
+# Fail build, if database update fails.
+res=`grep ERROR: db.log | awk '{ print $1 }' | tail -1`
+
+if [ "$res" = "ERROR:"  ]; then
+   echo "Database error!"
+   exit 1;
+else
+   echo "Database updated."
+   # exit 0;
+fi
+
