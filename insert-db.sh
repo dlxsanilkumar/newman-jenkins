7106bf014e46430f30e9df62c044630e92ca7b7a
Anil Kumar N <anil.n.kumar@delixus.com>
2021-12-08 11:28:19 +0530

    Modified insert-db.log

--- insert-db.sh
+++ insert-db.sh
@@ -11,7 +11,7 @@ case $GET_VULN_DATA in
 	 echo "--- insert into build_ids table ---"
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname_TEST, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'FAILURE');" > db.log 2>&1;
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM solidityscans ORDER BY scanid DESC LIMIT 1";
-	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1"; exit 1;
+	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1";
 	 ;;
      *)
      echo Build is completed successfully...
@@ -19,7 +19,7 @@ case $GET_VULN_DATA in
 	 echo "--- insert into build_ids table ---"
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "INSERT INTO build_ids (build_id, jobname, status) VALUES (${BUILD_NUMBER}, '${JOB_NAME}', 'SUCCESS');" > db.log 2>&1;
 	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM solidityscans ORDER BY scanid DESC LIMIT 1";
-	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1"; exit 0;
+	 psql -h 13.126.95.15 -p 5432 -U postgres -d solidityscan -w -c "SELECT * FROM build_ids ORDER BY id DESC LIMIT 1";
 	 ;;
 esac
 
