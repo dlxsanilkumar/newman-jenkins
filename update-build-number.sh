6d7775a50d97b2ea94e16f03c81a52688afa494a
Anil Kumar N <anil.n.kumar@delixus.com>
2021-12-09 17:16:21 +0530

    Scan with Migration.sol

--- update-build-number.sh
+++ update-build-number.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i 's/Migration/Migration-'$BUILD_NUMBER'/g' tests/Delixus.postman_collection.json
+sed -i 's/scanName=Migration/scanName=Migration-'$BUILD_NUMBER'/g' tests/Delixus.postman_collection.json
